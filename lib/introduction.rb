69cebff5d552ae4169dd61b72a32a00500eee812
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-12 19:37:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,8 @@ it "takes in an argument of a name and puts out a phrase with that name using st
   end
 end
 describe "#introduction_with_language" do
- it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do
+  it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do
+    
   
 
 
