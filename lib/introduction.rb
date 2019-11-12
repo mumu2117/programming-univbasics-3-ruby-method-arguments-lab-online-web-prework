926d2c4ca1bf01f04f296eafd29f3f01d380d659
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-12 19:38:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,8 @@ it "takes in an argument of a name and puts out a phrase with that name using st
 end
 describe "#introduction_with_language" do
   it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do
-    
+     expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in 
+     
   
 
 
